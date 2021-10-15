8aa64160ea4e98c348a727696f9b889ba4e6203d
Lppt12345 <aibiettoi19@gmail.com>
2021-10-15 14:46:22 +0700

    Week5

--- src/Week5/BalancedBracket.java
+++ src/Week5/BalancedBracket.java
@@ -1,7 +1,33 @@
 package Week5;
-
+import  java.util.Stack;
 public class BalancedBracket {
     public static String isBalanced(String s) {
-            return "";
+        Stack <Character> S = new Stack<>();
+        for (int i =0; i< s.length() ; i++ ){
+            char c = s.charAt(i);
+            switch (c){
+                case '{':
+                case '[':
+                case  '(':
+                    S.push(c);
+                    break;
+                case ']':
+                    if (S.isEmpty() || S.pop() != ']'){
+                        return "NO";
+                    }
+                    break;
+                case ')':
+                    if (S.isEmpty() || S.pop() != ')'){
+                        return "NO";
+                    }
+                    break;7
+                case '}':
+                    if (S.isEmpty() || S.pop() != '}'){
+                        return "NO";
+                    }
+                    break;
+            }
+        }
+        return S.isEmpty() ?  "YES" : "NO";
     }
 }
