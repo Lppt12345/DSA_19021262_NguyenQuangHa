e483ad95e9ed9acc69153a55f3246e2302fe1fa3
Lppt12345 <aibiettoi19@gmail.com>
2021-10-22 07:40:53 +0700

    Week6

--- src/Week6/InsertionSortP1.java
+++ src/Week6/InsertionSortP1.java
@@ -45,8 +45,27 @@ public class InsertionSortP1 {
         insertionSort1(n, arr);
     }
 
-//    public static void main (String [] args){
-//        int[] arr = new int[]{1, 3, 4, 5, 8, 2};
-//        insertionSort1(6,arr);
+// THAM KHAO
+//
+//    static void insertionSort1(int n, int[] arr) {
+//        int last = arr[n - 1], i = n - 2;
+//        while (i >= 0 && last < arr[i]) {
+//            arr[i + 1] = arr[i--];
+//            printArray(arr);
+//        }
+//        arr[i + 1] = last;
+//        printArray(arr);
+//    }
+//
+//    static void printArray(int[] arr) {
+//        System.out.println(String.join(
+//                " ",
+//                Arrays.stream(arr).mapToObj(String::valueOf).toArray(String[]::new)
+//        ));
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] arr = new int[] {1, 2, 3, 4, 5, 6, 7, 0};
+//        insertionSort1(8, arr);
 //    }
 }
