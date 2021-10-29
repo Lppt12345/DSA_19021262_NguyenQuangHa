556274953afb0f26f2418bb9ed6095d5d6f972b5
Lppt12345 <aibiettoi19@gmail.com>
2021-10-29 10:49:00 +0700

    Week7

--- src/Week7/CountingSort1.java
+++ src/Week7/CountingSort1.java
@@ -8,12 +8,11 @@ import java.util.Scanner;
 public class CountingSort1 {
     public static List<Integer> countingSort(List<Integer> arr) {
         // Write your code here
-        List <Integer> result = new ArrayList<>(Collections.nCopies(100, 0));
+        List <Integer> count = new ArrayList<>(Collections.nCopies(100, 0));
         for (int i =0 ; i<arr.size(); i++ ){
-            int index = arr.get(i);
-            result.set(index, result.get(index) + 1);
+            count.set(arr.get(i), count.get(arr.get(i)) + 1);
         }
-        return result;
+        return count;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
