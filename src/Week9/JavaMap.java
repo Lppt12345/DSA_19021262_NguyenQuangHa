b7c1e560eed0c839cb2a91d4d2cd85b96ad09d93
Lppt12345 <aibiettoi19@gmail.com>
2021-11-12 09:31:52 +0700

    Week9 JavaMap

---
+++ src/Week9/JavaMap.java
@@ -0,0 +1,23 @@
+package Week9;
+
+public class JavaMap {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        Map<String, String> phoneBook = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String name = scan.nextLine();
+            String number = scan.nextLine();
+            phoneBook.put(name, number);
+        }
+        while (scan.hasNextLine()) {
+            String name = scan.nextLine();
+            if (phoneBook.containsKey(name)) {
+                System.out.println(name + "=" + phoneBook.get(name));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+        scan.close();
+    }
+}
