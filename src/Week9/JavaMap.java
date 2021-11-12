5b8fd6b5e4e58fe3c2528a9a3ac38cb1d8ced4d4
Lppt12345 <aibiettoi19@gmail.com>
2021-11-12 09:36:37 +0700

    Week9 JavaMap

--- src/Week9/JavaMap.java
+++ src/Week9/JavaMap.java
@@ -1,5 +1,9 @@
 package Week9;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class JavaMap {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
